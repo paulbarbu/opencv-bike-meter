090bf6f9708552838b464574e5f463406f940f49
Barbu Paul - Gheorghe <barbu.paul.gheorghe@gmail.com>
2020-06-02 20:54:50 +0300

    use live video from a connected camera and clean the code a bit as well as add some docs in README.md

--- README.md
+++ README.md
@@ -3,15 +3,41 @@ Bike-Meter
 A simple speed and distance bike computer built using OpenCV and Python 3.
 
 If you're using your bike on a home trainer and don't have the necessary speed sensors on it, this might help.
-It uses OpenCV to count your back wheel's RPM and from there it computes the speed and distance
-travelled for the current training session.
+It uses OpenCV to count your back wheel's rotations and from there it computes the speed and distance
+traveled for the current training session.
 
-So you need to place your laptop/RPi/webcam beside the back wheel and to have a reflective sticker or
-safety element on the wheel. Anything that spins with the wheel and is a bit different from it should work.
+So you need to place your laptop/RPi/webcam beside the back wheel and to have a reflective or coloured sticker or safety element on the wheel.
+Anything that spins with the wheel and is a bit different from it and the background should work.
+Aim your camera only at a part of the wheel, so that the sticker/reflector gets in and out of frame. this is the working principle of this application. Whenever the sticker appears in frame try to detect it and I count a rotation. Knowing the size of the wheel I can compute the distance traveled. Keeping track of the time elapsed speed can be computed as well.
+
+*NOTE:* Make sure nothing will get stuck to your wheel, cables and such. Do not damage your equipment or yourself.
+
+Also note that the results and performance of this program may vary with lightning conditions, camera quality (although I have used a crappy one and it worked), object size, color and positioning.
+See the example picture and video in this repository for demo.
 
 Usage
 =====
-TODO
+Install the required packages:
+`pip install -r requirements.txt`
+
+Configure your threshold levels, place the reflective element in front of the camera and capture an image or a video (then choose one of the frames of the video), then:
+
+`range-detector -f HSV -p -i reflector_in_motion.jpg`
+
+After you've isolated the reflective/coloured element from your wheel, plug the numbers in
+the `REFLECTOR_LOWER_HSV` and `REFLECTOR_UPPER_HSV` variables in the code.
+
+Also you may need to change the `WHEEL_DIAMETER` to reflect you wheel's diameter in millimeters for accurate computations.
+
+Finally you may need to tweak `RADIUS_THRESHOLD` to an appropriate number for your object,
+camera and distance.
+If no rotation or too many rotations are detected, you need to increase or decrease this number (it's in pixels).
+
+Run, either with a live camera (`ls /dev/ | grep video` and get the number):
+`python bike-meter.py -v 2`
+
+Or with a pre-recorded video:
+`python bike-meter.py -v v1.mp4`
 
 LICENSE
 =======
--- bike-meter.py
+++ bike-meter.py
@@ -1,8 +1,7 @@
 # inspiration for object tracking from Adrian Rosebrock:
 # https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
 
-from imutils.video import VideoStream
-import numpy as np
+import argparse
 import cv2
 import imutils
 import math
@@ -28,30 +27,64 @@ prev_reflector = False # did we find the reflector in the previous frame?
 current_reflector = False # did we find the reflector in the current frame?
 rotations = 0 # number of rotations we counted up to now
 frames = 0 # number of frames read up to now
+time_elapsed = 0 # time in seconds since we started processing
+minutes_elapsed = 0 # just another measure for time in order to display it user friendly
+seconds_elapsed = 0 # just another measure for time in order to display it user friendly
 
 instant_rotations = 0 # the number of rotations for the "instantaneous" speed computation
 instant_speed = 0 # km/h
 instant_distance = 0 # the distance traveled for the "instantaneous" speed computation
 max_speed = 0 # km/h - max speed for the whole session
 
-video = cv2.VideoCapture('v1.mp4')
+MAX_FRAME_WIDTH = 640 # pixels
 
-FRAME_RATE = video.get(cv2.CAP_PROP_FPS)
+parser = argparse.ArgumentParser('Process a video file or camera input in order to get the speed and distance traveled by bike on an indoor trainer')
+parser.add_argument('-v', '--video', default=0, help='video file path or device number (ls /dev/ | grep video), if not passed the default camera will be used')
+parser.add_argument('-f', '--fps', type=int, default=30, help='frames per second of the video or camera, it defaults to 30 if the FPS cannot be deduced from the video or to what the value of this argument is')
+args = parser.parse_args()
 
-while video.isOpened():
-    retval, frame_small = video.read()
+device_or_file = args.video
+try:
+    device_or_file = int(args.video)
+except ValueError:
+    pass
+
+video = cv2.VideoCapture(device_or_file)
+
+if not video:
+    print('Invalid video file/device')
+    exit(0)
+
+frame_rate = video.get(cv2.CAP_PROP_FPS)
+if math.isnan(frame_rate):
+    frame_rate = args.fps
+#print('Detected frame rate: {}'.format(frame_rate))
+
+resize = False # mark if the frame size is too large
+width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+#print('Frame width: {}'.format(width))
+
+if width > MAX_FRAME_WIDTH:
+    resize = True
+
+while video and video.isOpened():
+    retval, frame = video.read()
     frames += 1
 
-    if not retval or frame_small is None:
+    if not retval or frame is None:
         break
-
-    cv2.imshow('Bike-meter', frame_small)
 
     key = cv2.waitKey(1)
     if key == ord(' '):
         break
 
-    #frame_small = imutils.resize(frame, width=600) # resize to reduce the time for computations
+    if resize:
+        # resize to reduce the time for computations
+        frame_small = imutils.resize(frame, width=MAX_FRAME_WIDTH)
+    else:
+        frame_small = frame
+
+    cv2.imshow('Bike-meter', frame_small)
     blurred = cv2.GaussianBlur(frame_small, (11, 11), 0) # reduce noise in the image, we don't need all the details
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV) # work in HSV to be able to threshold what we see
 
@@ -80,7 +113,7 @@ while video.isOpened():
             #print('contour at center={}, radius={}'.format((x, y), r))
             # OpenCV is by default in the (B, G, R) format, so this is RED
             cv2.circle(frame_small, (int(x), int(y)), int(r), (0, 0, 255))
-            cv2.imshow('Bike-meter', imutils.resize(frame_small))
+            cv2.imshow('Bike-meter', frame_small)
         else:
             # the enclosing circle was too small, no reflector found
             current_reflector = False
@@ -101,23 +134,36 @@ while video.isOpened():
     prev_reflector = current_reflector
 
     # this is how we keep track of time, knowing the frame rate of the captured video and how many frames we processed until now
-    time_elapsed = frames/FRAME_RATE
+    time_elapsed = frames/frame_rate
     distance = rotations * WHEEL_CIRCUMFERENCE # km
     avg_speed = (distance / time_elapsed) * (60*60) # km/h
 
     # only update the instantaneous speed once every INSTANT_FRAME_QUANTA
     if frames % INSTANT_FRAME_QUANTA == 0:
         instant_distance = instant_rotations * WHEEL_CIRCUMFERENCE # km
-        instant_speed = instant_distance / (INSTANT_FRAME_QUANTA/FRAME_RATE) * (60*60) # km/h
+        instant_speed = instant_distance / (INSTANT_FRAME_QUANTA/frame_rate) * (60*60) # km/h
         instant_rotations = 0
 
         if instant_speed > max_speed:
             max_speed = instant_speed
 
-    print('num rotations: {}; time: {:.2f} s; avg rot/s: {:.2f}; avg rpm: {:.2f}; distance: {:.2f} km; avg speed: {:.2f} km/h'.format(rotations, time_elapsed, rotations/time_elapsed, (rotations/time_elapsed)*60, distance, avg_speed))
-    print('instant rot: {}; instant distance: {:.2f} km; instant speed: {:.2f} km/h'.format(instant_rotations, instant_distance, instant_speed))
-    print('max speed: {:.2f} km/h'.format(max_speed))
-    print('\033[F\033[F\033[F\033[F') # move up 4 lines to delete what I just displayed
+    minutes_elapsed = int(time_elapsed / 60)
+    seconds_elapsed = int(time_elapsed % 60)
+
+    # \033[K - erase line contents
+    # \033[F - move cursor up one line
+    print('\033[Knum rotations: {:3d}; time: {:02d}:{:02d} mm:ss; avg rot/s: {:3.2f}; avg rpm: {:3.2f}; distance: {:2.2f} km; avg speed: {:2.2f} km/h'.format(rotations, minutes_elapsed, seconds_elapsed, rotations/time_elapsed, (rotations/time_elapsed)*60, distance, avg_speed))
+    print('\033[Kinstant rot: {:3d}; instant distance: {:2.2f} km; instant speed: {:2.2f} km/h'.format(instant_rotations, instant_distance, instant_speed))
+    print('\033[Kmax speed: {:2.2f} km/h'.format(max_speed))
+    print('\033[F\033[F\033[F\033[F') # move up 4 lines so that next time I overwrite what I just displayed
+
+#print('time_elapsed: {}'.format(time_elapsed))
+if video and time_elapsed > 0:
+    print('\033[Knum rotations: {:3d}; time: {:02d}:{:02d} mm:ss; avg rot/s: {:3.2f}; avg rpm: {:3.2f}; distance: {:2.2f} km; avg speed: {:2.2f} km/h'.format(rotations, minutes_elapsed, seconds_elapsed, rotations/time_elapsed, (rotations/time_elapsed)*60, distance, avg_speed))
+    print('\033[Kinstant rot: {:3d}; instant distance: {:2.2f} km; instant speed: {:2.2f} km/h'.format(instant_rotations, instant_distance, instant_speed))
+    print('\033[Kmax speed: {:2.2f} km/h'.format(max_speed))
+else:
+    print('video might not have been opened')
 
 video.release()
 cv2.destroyAllWindows()
